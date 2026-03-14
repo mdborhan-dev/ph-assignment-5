41768da19cab0d323450021390f4e944b12e1258
mdborhan-dev <dev.mdborhan@gmail.com>
2026-03-15 03:14:36 +0600

    new issue functionality added

--- script/home.js
+++ script/home.js
@@ -14,6 +14,25 @@ const modalCreateDate = document.getElementById("modalCreateDate");
 const modalAuthor = document.getElementById("modalAuthor");
 const modalBadge = document.getElementById("modalBadge");
 const modalTitle = document.getElementById("modalTitle");
+const newIssueButton = document.getElementById("newIssue");
+const newModal = document.getElementById("newModal");
+const newIssueTitle = document.getElementById("newIssueTitle");
+const newIssueDescription = document.getElementById("newIssueDescription");
+const newIssuePirority = document.getElementById("newIssuePirority");
+const labelBug = document.getElementById("labelBug");
+const labelHelpWanted = document.getElementById("labelHelpWanted");
+const labelEnhancement = document.getElementById("labelEnhancement");
+const labelDocumentation = document.getElementById("labelDocumentation");
+const labelFirstIssue = document.getElementById("labelFirstIssue");
+const newIssueAuthor = document.getElementById("newIssueAuthor");
+const createIssueBtn = document.getElementById("createIssueBtn");
+const modalUPdated = document.getElementById("Updated");
+let localIssues = [];
+
+newIssueButton.addEventListener("click", () => {
+  // window.location.assign("./newIssue.html");
+  newModal.showModal();
+});
 
 let issueArray = [];
 
@@ -52,19 +71,23 @@ function buttonColor(id) {
 
 ClosedButton.addEventListener("click", () => {
   showLoading();
-  const closedIssues = issueArray.filter((issue) => issue.status === "closed");
+  const allIssues = [...issueArray, ...localIssues];
+  const closedIssues = allIssues.filter((issue) => issue.status === "closed");
   displayAllIssues(closedIssues);
   hideLoading();
 });
+
 openedButton.addEventListener("click", () => {
   showLoading();
-  const OpenedIssues = issueArray.filter((issue) => issue.status === "open");
-  displayAllIssues(OpenedIssues);
+  const allIssues = [...issueArray, ...localIssues];
+  const openedIssues = allIssues.filter((issue) => issue.status === "open");
+  displayAllIssues(openedIssues);
   hideLoading();
 });
+
 allButton.addEventListener("click", () => {
   showLoading();
-  displayAllIssues(issueArray);
+  displayAllIssues([...issueArray, ...localIssues]);
   hideLoading();
 });
 
@@ -140,14 +163,26 @@ searchBox.addEventListener("input", async (event) => {
   } else {
     loadAllIssues();
   }
+  if (issuesCount.innerText == "0") {
+    cardContainer.innerHTML = `<h2 class="text-center text-4xl font-bold col-span-4 p-10">No Matching Issues</h2>`;
+  }
 });
 
 async function openModal(issueId) {
-  const res = await fetch(
-    `https://phi-lab-server.vercel.app/api/v1/lab/issue/${issueId}`,
-  );
-  const data = await res.json();
-  const cardDetails = data.data;
+  let cardDetails;
+
+  const localIssue = localIssues.find((issue) => issue.id === issueId);
+  // console.log(localIssue);
+  if (localIssue) {
+    cardDetails = localIssue;
+  } else {
+    const res = await fetch(
+      `https://phi-lab-server.vercel.app/api/v1/lab/issue/${issueId}`,
+    );
+    const data = await res.json();
+    cardDetails = data.data;
+  }
+
   const labels = cardDetails.labels
     .map((label) => {
       let colorClass = "primary";
@@ -165,6 +200,7 @@ async function openModal(issueId) {
       return `<span class="px-4 rounded-full py-[6px] border-[1px] ${colorClass}">${label}</span>`;
     })
     .join(" ");
+
   modalTitle.innerText = cardDetails.title;
   modalDescription.innerText = cardDetails.description;
   modalAuthor.innerText = cardDetails.author;
@@ -177,6 +213,7 @@ async function openModal(issueId) {
   modalLables.innerHTML = `${labels}`;
   modalPriority.innerHTML = cardDetails.priority;
   modalPriority.className = `badge ${cardDetails.priority == "high" ? "badge-secondary" : cardDetails.priority == "medium" ? "badge-warning" : cardDetails.priority == "low" ? "badge-primary" : "NO priority set"} rounded-full`;
+  modalUPdated.innerText = `${new Date(cardDetails.updatedAt).toLocaleDateString("en-US")}`;
   cardModalOpen.showModal();
 }
 
@@ -186,6 +223,91 @@ searchBox.addEventListener("click", () => {
   searchBox.classList.remove(`max-sm:w-1`);
 });
 
-if (issues.length === 0) {
-  container.innerHTML = "No issues found";
-}
+createIssueBtn.addEventListener("click", () => {
+  let issue = {
+    id: "",
+    title: "",
+    description: "",
+    priority: "",
+    labels: [],
+    author: "",
+    createdAt: "",
+    status: "open",
+    updatedAt: "",
+    assignee: "",
+  };
+  issue.title = newIssueTitle.value;
+  issue.description = newIssueDescription.value;
+  issue.priority =
+    newIssuePirority.value != "Choose from here" ? newIssuePirority.value : "";
+  labelBug.checked ? issue.labels.push("bug") : "";
+  labelHelpWanted.checked ? issue.labels.push("help wanted") : "";
+  labelEnhancement.checked ? issue.labels.push("enhancement") : "";
+  labelDocumentation.checked ? issue.labels.push("documentation") : "";
+  labelFirstIssue.checked ? issue.labels.push("good first issue") : "";
+  issue.author = newIssueAuthor.value;
+  issue.id = Number(issuesCount.innerText) + 1;
+  issue.createdAt = new Date().toISOString();
+  issue.updatedAt = new Date().toISOString();
+
+  // console.log(issue);
+  // console.log(issue.labels);
+
+  const labels = issue.labels
+    .map((label) => {
+      let colorClass = "primary";
+      if (label.toLowerCase() === "bug")
+        colorClass = "badge badge-outline badge-error badge-lg";
+      if (label.toLowerCase() === "help wanted")
+        colorClass = "badge badge-outline badge-warning badge-lg";
+      if (label.toLowerCase() === "good first issue")
+        colorClass = "badge badge-outline badge-primary badge-lg";
+      if (label.toLowerCase() === "enhancement")
+        colorClass = "badge badge-outline badge-accent badge-lg";
+      if (label.toLowerCase() === "documentation")
+        colorClass = "badge badge-outline badge-info badge-lg";
+
+      return `<span class="px-4 rounded-full py-[6px] border-[1px] ${colorClass}">${label}</span>`;
+    })
+    .join(" ");
+  const div = document.createElement("div");
+  div.className = `card p-4 bg-white rounded-md shadow-sm border-t-4 space-y-3 border-[${issue.status == "open" ? "#00A96E" : "#A855F7"}]`;
+  div.innerHTML = `
+                  <div onclick="openModal(${issue.id})" id="topOfCard" class="flex justify-between">
+              <div class='flex'><img
+                src="./assets/${issue.status == "open" ? "Open-Status.png" : "Closed-Status.png"}"
+                alt="status image"
+                class="w-[100%] rounded-full"
+              />
+              <h2 class='${issue.status == "open" ? "badge badge-soft badge-lg badge-success" : "badge badge-soft badge-lg badge-error"}'>${issue.status}</h2>
+              </div>
+<div onclick="openModal(${issue.id})" class="badge rounded-full ${issue.priority == "high" ? "badge-secondary" : issue.priority == "medium" ? "badge-warning" : issue.priority == "low" ? "badge-primary" : "NO priority set"}">${issue.priority}</div>
+            </div>
+            <div id="middleOfCard" class="space-y-3">
+              <h2 onclick="openModal(${issue.id})" class="font-semibold text-[20px]">
+                ${issue.title}
+              </h2>
+              <p onclick="openModal(${issue.id})" class="text-[#64748B] loading-2">
+              ${issue.description}
+              </p>
+              <div onclick="openModal(${issue.id})">
+                ${labels}
+              </div>
+            </div>
+            <hr class="border-gray-400 mt-6" />
+            <div onclick="openModal(${issue.id})" id="bottomOfCard" class="space-y-2 text-[14px">
+              <div onclick="openModal(${issue.id})" class="flex justify-between">
+                <p id="author" class="text-[#64748B]">#${issue.id} by ${issue.author}</p>
+                <p id="date" class="text-[#64748B]">${new Date(issue.createdAt).toLocaleDateString("en-US")}</p>
+              </div>
+            </div>
+            <div onclick="openModal(${issue.id})" class="flex justify-between">
+              <p id="assignee" class="text-[#64748B]">Assignee: ${issue.assignee ? issue.assignee : "No assignee"}</p>
+              <p id="update" class="text-[#64748B]">Updated: ${new Date(issue.updatedAt).toLocaleDateString("en-US")}</p>
+            </div>
+          </div>
+      `;
+  cardContainer.appendChild(div);
+  issuesCount.innerText = Number(issuesCount.innerText) + 1;
+  localIssues.push(issue);
+});
